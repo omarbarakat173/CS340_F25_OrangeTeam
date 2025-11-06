b49825cd3377fe3cbcecdbbaa3cf1283c0cc0201
Hunter-Merritt <tzhgc9zttq@privaterelay.appleid.com>
2025-11-06 14:31:19 -0600

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,1 +1,7 @@
-
+class config:
+    TAX_RATE = 0.08
+    DISCOUNT_RATE = 0.10
+    CURRENCY = "USD"
+    PRODUCT_CSV_PATH = "Input/products.csv"
+    CART_PICKLE_PATH = "Input/cart.pkl"
+    OUTPUT_FOLDER = "Output/"
